c255b408d81ca84fe60424a2b6be60ba010701d4
PQCraft <0456523@gmail.com>
2022-10-11 15:24:12 -0400

    UI code progress

--- src/renderer/ui.h
+++ src/renderer/ui.h
@@ -3,21 +3,27 @@
 
 #include <stdbool.h>
 
+struct ui_elem_property {
+    char* name;
+    char* value;
+};
+
 struct ui_elem {
     bool valid;
     int type;
     char* name;
+    int parent;
     int children;
     int* childdata;
     int properties;
-    char** propertydata;
+    struct ui_elem_property* propertydata;
 };
 
-int newElem(int, char*, int, ...);
-void editElem(int, char* /*name*/, int /*parent*/, ... /*properties*/);
-void deleteElem(int);
-struct ui_elem* getElemData(int);
-int getElemByName(char*, bool /*last*/);
-int* getElemsByName(char*, int* /*count*/);
+int newElem(int /*type*/, char* /*name*/, int /*parent*/, ... /*properties*/);
+void editElem(int /*id*/, char* /*name*/, int /*parent*/, ... /*properties*/);
+void deleteElem(int /*id*/);
+struct ui_elem* getElemData(int /*id*/);
+int getElemByName(char* /*name*/, bool /*reverse*/);
+int* getElemsByName(char* /*name*/, int* /*count*/);
 
 #endif
