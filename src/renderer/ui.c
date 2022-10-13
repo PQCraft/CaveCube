a1adc4a32aac55146d48f0b3c432bdbf062b8faa
PQCraft <0456523@gmail.com>
2022-10-12 23:32:14 -0400

    UI code progress

--- src/renderer/ui.c
+++ src/renderer/ui.c
@@ -4,13 +4,16 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+float ui_scale = 2.0;
+
 static int elems = 0;
 static struct ui_elem* elemdata;
 
+#define idValid(x) (x >= 0 && x < elem)
+#define elemValid(x) (idValid(x) && elemdata[x].valid)
+
 int newElem(int type, char* name, int parent, ...) {
-    if (parent >= 0) {
-        if (parent >= elems || !elemdata[parent].valid) return -1;
-    }
+    if (!elemValid(parent)) return -1;
     int index = -1;
     for (int i = 0; i < elems; ++i) {
         if (!elemdata[i].valid) {index = i; break;}
@@ -55,13 +58,13 @@ int newElem(int type, char* name, int parent, ...) {
 }
 
 void editElem(int id, char* name, int parent, ...) {
-    if (id >= elems || !elemdata[id].valid) return;
+    if (!elemValid(id)) return;
     struct ui_elem* e = &elemdata[id];
     if (name) {
         if (*name) {free(e->name); e->name = strdup(name);}
         else {free(e->name); e->name = NULL;}
     }
-    if (parent >= 0 && parent < elems && elemdata[parent].valid) e->parent = parent;
+    if (elemValid(parent)) e->parent = parent;
     va_list args;
     va_start(args, parent);
     while (1) {
@@ -108,18 +111,20 @@ void editElem(int id, char* name, int parent, ...) {
 }
 
 void deleteElem(int id) {
-    if (id >= elems || !elemdata[id].valid) return;
+    if (!elemValid(id)) return;
     struct ui_elem* e = &elemdata[id];
-    struct ui_elem* p = &elemdata[e->parent];
     e->valid = false;
-    for (int i = 0; i < p->children; ++i) {
-        if (p->childdata[i] == id) {
-            p->childdata[i] = -1;
-            break;
+    if (elemValid(e->parent)) {
+        struct ui_elem* p = &elemdata[e->parent];
+        for (int i = 0; i < p->children; ++i) {
+            if (p->childdata[i] == id) {
+                p->childdata[i] = -1;
+                break;
+            }
         }
     }
     for (int i = 0; i < e->children; ++i) {
-        if (p->childdata[i] >= 0) deleteElem(e->childdata[i]);
+        if (e->childdata[i] >= 0) deleteElem(e->childdata[i]);
     }
     free(e->name);
     free(e->childdata);
@@ -131,31 +136,18 @@ void deleteElem(int id) {
 }
 
 struct ui_elem* getElemData(int id) {
-    if (id >= elems || !elemdata[id].valid) return NULL;
-    return &elemdata[id];
+    return (elemValid(id)) ? &elemdata[id] : NULL;
 }
 
 int getElemByName(char* name, bool reverse) {
     int i;
-    if (reverse) {
-        i = elems - 1;
-    } else {
-        i = 0;
-    }
+    if (reverse) {i = elems - 1;} else {i = 0;}
     while (1) {
-        if (reverse) {
-            if (i < 0) break;
-        } else {
-            if (i >= elems) break;
-        }
+        if (reverse) {if (i < 0) break;} else {if (i >= elems) break;}
         if (elemdata[i].valid && !strcasecmp(elemdata[i].name, name)) {
             return i;
         }
-        if (reverse) {
-            --i;
-        } else {
-            ++i;
-        }
+        if (reverse) {--i;} else {++i;}
     }
     return -1;
 }
@@ -166,12 +158,51 @@ int* getElemsByName(char* name, int* _count) {
     output[0] = -1;
     for (int i = 0; i < elems; ++i) {
         if (elemdata[i].valid && !strcasecmp(elemdata[i].name, name)) {
-            ++count;
+            output[count++] = i;
             output = realloc(output, (count + 1) * sizeof(*output));
-            output[count - 1] = i;
             output[count] = -1;
         }
     }
     if (_count) *_count = count;
     return output;
+}
+
+static inline char* getProp(struct ui_elem* e, char* name) {
+    for (int i = 0; i < e->properties; ++i) {
+        if (e->propertydata[i].name && e->propertydata[i].value && *e->propertydata[i].value) {
+            if (!strcasecmp(name, e->propertydata[i].name)) return e->propertydata[i].value;
+        }
+    }
+}
+
+static inline float getSize(char* propval, float max) {
+    float num = 0;
+    char suff = 0;
+    sscanf(propval, "%f%c", &num, &suff);
+    if (suff == '%') {
+        num = max * num * 0.01;
+    }
+}
+
+static inline void calcProp(int id) {
+    if (!elemValid(id)) return;
+    struct ui_elem* e = &elemdata[id];
+    struct ui_elem_calcprop p_prop;
+    if (elemValid(e->parent)) {
+        p_prop = elemdata[e->parent].calcprop;
+    } else {
+        p_prop = {
+            .x = 0,
+            .y = 0,
+            .width = rendinf.width,
+            .height = rendinf.height,
+            .z = 0.0
+        };
+    }
+    {
+        char* margin_x = getProp(e, "margin_x");
+        char* margin_y = getProp(e, "margin_y");
+        p_prop.width -= (margin_x) ? atoi(margin_x) * 2 : 0;
+        p_prop.height -= (margin_y) ? atoi(margin_y) * 2 : 0;
+    }
 }
