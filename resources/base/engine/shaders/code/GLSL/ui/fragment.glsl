6fa0ca1468b4671ac8360a1ac7fd0144ff40e16b
PQCraft <0456523@gmail.com>
2022-11-01 00:26:30 -0400

    MSYS2 test

--- resources/base/engine/shaders/code/GLSL/ui/fragment.glsl
+++ resources/base/engine/shaders/code/GLSL/ui/fragment.glsl
@@ -1,13 +1,17 @@
 in vec2 texCoord;
-in float chr;
+in float texNum;
 in vec4 mcolor;
 in vec4 bmcolor;
-in uint texIndex;
+flat in uint texIndex;
 uniform sampler2DArray texData[2];
 
 out vec4 fragColor;
 
 void main() {
-    fragColor = mcolor * texture(texData[texIndex], vec3(texCoord, chr));
+    if (bool(texIndex)) {
+        fragColor = mcolor * texture(texData[1], vec3(texCoord, texNum));
+    } else {
+        fragColor = mcolor * texture(texData[0], vec3(texCoord, texNum));
+    }
     fragColor = mix(bmcolor, fragColor, fragColor.a);
 }
--- resources/base/engine/shaders/code/GLSL/ui/vertex.glsl
+++ resources/base/engine/shaders/code/GLSL/ui/vertex.glsl
@@ -3,28 +3,39 @@ layout (location = 0) in uint data1;
 // text:    [16 bits: x][16 bits: y]
 layout (location = 1) in uint data2;
 // ui elem: [1 bit: 0][7 bits: reserved][8 bits: elem id][8 bits: alpha][8 bits: reserved]
-// text:    [1 bit: 1][5 bits: reserved][1 bit: texture y][1 bit: texture x][8 bits: text char][8 bits: reserved][8 bits: reserved]
+// text:    [1 bit: 1][5 bits: reserved][1 bit: texture x][1 bit: texture y][8 bits: text char][8 bits: reserved][8 bits: reserved]
 layout (location = 2) in uint data3;
 // ui elem: [16 bits: texture x][16 bits: texture y]
 // text:    [8 bits: fgc alpha][8 bits: bgc alpha][4 bits: text fgc][4 bits: text bgc][8 bits: reserved]
 uniform float xsize;
 uniform float ysize;
 uniform vec3 textColor[16];
+uniform vec2 uitexdiv;
 
 out vec2 texCoord;
-out float chr;
+out float texNum;
 out vec4 mcolor;
 out vec4 bmcolor;
+flat out uint texIndex;
 
 void main() {
-    if (((data2 >> 31) & uint(1)) == uint(1)) {
-        chr = float((data2 >> 16) & uint(255));
-        texCoord.x = float((data2 >> 15) & uint(1));
-        texCoord.y = float((data2 >> 14) & uint(1));
+    if ((texIndex = ((data2 >> 31) & uint(1))) == uint(1)) {
         float x = float(int((data1 >> 16) & uint(65535))) / xsize * 2.0 - 1.0;
         float y = 1.0 - float(int(data1 & uint(65535))) / ysize * 2.0;
         gl_Position = vec4(x, y, 0.0, 1.0);
+        texNum = float((data2 >> 16) & uint(255));
+        texCoord.x = float((data2 >> 25) & uint(1));
+        texCoord.y = float((data2 >> 24) & uint(1));
+        mcolor = vec4(textColor[uint((data3 >> 12) & uint(15))], float((data3 >> 24) & uint(8)) / 255.0);
+        bmcolor = vec4(textColor[uint((data3 >> 8) & uint(15))], float((data3 >> 16) & uint(8)) / 255.0);
     } else {
-        gl_Position
+        float x = float(int((data1 >> 16) & uint(65535))) / xsize * 2.0 - 1.0;
+        float y = 1.0 - float(int(data1 & uint(65535))) / ysize * 2.0;
+        gl_Position = vec4(x, y, 0.0, 1.0);
+        texNum = float((data2 >> 16) & uint(255));
+        texCoord.x = float((data3 >> 16) & uint(65535)) / uitexdiv.x;
+        texCoord.y = float((data3) & uint(65535)) / uitexdiv.y;
+        mcolor = vec4(1.0, 1.0, 1.0, float((data2 >> 8) & uint(8)) / 255.0);
+        bmcolor = vec4(1.0, 1.0, 1.0, 0.0);
     }
 }
