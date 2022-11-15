0c1b5a66854ad3adb5fb36d3c9d92737aaf753f3
PQCraft <0456523@gmail.com>
2022-11-14 21:10:46 -0500

    Update CGLM

--- src/renderer/cglm/struct/clipspace/project_zo.h
+++ src/renderer/cglm/struct/clipspace/project_zo.h
@@ -5,13 +5,19 @@
  * Full license can be found in the LICENSE file
  */
 
-#ifndef cglm_project_zo_h
-#define cglm_project_zo_h
+/*
+ Functions:
+   CGLM_INLINE vec3s glms_unprojecti_no(vec3s pos, mat4s invMat, vec4s vp, vec3 dest)
+   CGLM_INLINE vec3s glms_project_no(vec3s pos, mat4s m, vec4s vp, vec3s dest)
+ */
 
-#include "../common.h"
-#include "../vec3.h"
-#include "../vec4.h"
-#include "../mat4.h"
+#ifndef cglms_project_zo_h
+#define cglms_project_zo_h
+
+#include "../../common.h"
+#include "../../types-struct.h"
+#include "../../plane.h"
+#include "../../cam.h"
 
 /*!
  * @brief maps the specified viewport coordinates into specified space [1]
@@ -34,24 +40,18 @@
  *   glm_mat4_mul(viewProj, model, MVP);
  *   glm_mat4_inv(viewProj, invMVP);
  *
- * @param[in]  pos      point/position in viewport coordinates
+ * @param[in]  pos          point/position in viewport coordinates
  * @param[in]  invMat   matrix (see brief)
- * @param[in]  vp       viewport as [x, y, width, height]
- * @param[out] dest     unprojected coordinates
+ * @param[in]  vp            viewport as [x, y, width, height]
+ *
+ * @returns unprojected coordinates
  */
 CGLM_INLINE
-void
-glm_unprojecti_zo(vec3 pos, mat4 invMat, vec4 vp, vec3 dest) {
-  vec4 v;
-
-  v[0] = 2.0f * (pos[0] - vp[0]) / vp[2] - 1.0f;
-  v[1] = 2.0f * (pos[1] - vp[1]) / vp[3] - 1.0f;
-  v[2] = pos[2];
-  v[3] = 1.0f;
-
-  glm_mat4_mulv(invMat, v, v);
-  glm_vec4_scale(v, 1.0f / v[3], v);
-  glm_vec3(v, dest);
+vec3s
+glms_unprojecti_zo(vec3s pos, mat4s invMat, vec4s vp, vec3 dest) {
+  vec3s dest;
+  glm_unprojecti_zo(pos.raw, invMat.raw, vp.raw, dest.raw);
+  return dest;
 }
 
 /*!
@@ -62,27 +62,17 @@ glm_unprojecti_zo(vec3 pos, mat4 invMat, vec4 vp, vec3 dest) {
  *   glm_mat4_mul(viewProj, model, MVP);
  *
  * @param[in]  pos      object coordinates
- * @param[in]  m        MVP matrix
- * @param[in]  vp       viewport as [x, y, width, height]
- * @param[out] dest     projected coordinates
+ * @param[in]  m          MVP matrix
+ * @param[in]  vp        viewport as [x, y, width, height]
+ *
+ * @returns projected coordinates
  */
 CGLM_INLINE
-void
-glm_project_zo(vec3 pos, mat4 m, vec4 vp, vec3 dest) {
-  CGLM_ALIGN(16) vec4 pos4;
-
-  glm_vec4(pos, 1.0f, pos4);
-
-  glm_mat4_mulv(m, pos4, pos4);
-  glm_vec4_scale(pos4, 1.0f / pos4[3], pos4); /* pos = pos / pos.w */
-
-  dest[2] = pos4[2];
-  
-  glm_vec4_scale(pos4, 0.5f, pos4);
-  glm_vec4_adds(pos4,  0.5f, pos4);
-
-  dest[0] = pos4[0] * vp[2] + vp[0];
-  dest[1] = pos4[1] * vp[3] + vp[1];
+vec3s
+glms_project_zo(vec3s pos, mat4s m, vec4s vp, vec3 dest) {
+  vec3s dest;
+  glm_project_zo(pos.raw, m.raw, vp.raw, dest.raw);
+  return dest;
 }
 
 /*!
@@ -92,20 +82,15 @@ glm_project_zo(vec3 pos, mat4 m, vec4 vp, vec3 dest) {
  *   glm_mat4_mul(proj, view, viewProj);
  *   glm_mat4_mul(viewProj, model, MVP);
  *
- * @param[in]  v  object coordinates
- * @param[in]  m  MVP matrix
+ * @param[in]  v object coordinates
+ * @param[in]  m MVP matrix
  *
  * @returns projected z coordinate
  */
 CGLM_INLINE
-float
-glm_project_z_zo(vec3 v, mat4 m) {
-  float z, w;
-
-  z = m[0][2] * v[0] + m[1][2] * v[1] + m[2][2] * v[2] + m[3][2];
-  w = m[0][3] * v[0] + m[1][3] * v[1] + m[2][3] * v[2] + m[3][3];
-
-  return z / w;
+vec3s
+glms_project_z_zo(vec3s v, mat4s m) {
+  return glm_project_z_zo(v.raw, m.raw);
 }
 
 #endif /* cglm_project_zo_h */
