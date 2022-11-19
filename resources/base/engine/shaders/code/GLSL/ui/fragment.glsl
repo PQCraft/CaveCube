b9466aa90cf7e077f2f76f8b5ea85ec56a6fccb9
PQCraft <0456523@gmail.com>
2022-11-18 21:31:53 -0500

    Progress update

--- resources/base/engine/shaders/code/GLSL/ui/fragment.glsl
+++ resources/base/engine/shaders/code/GLSL/ui/fragment.glsl
@@ -1,15 +1,34 @@
-in vec2 texCoord;
 in float texNum;
 in vec4 mcolor;
 in vec4 bmcolor;
+in float chartw;
+in float charth;
+in float chartx;
+in float charty;
+uniform float scale;
 flat in uint elemType;
+flat in uint b;
+flat in uint u;
+flat in uint s;
 uniform sampler2DArray fontTexData;
 
 out vec4 fragColor;
 
 void main() {
+    vec2 texCoord = vec2(chartx / chartw, charty / charth);
     if (elemType == uint(1)) {
-        fragColor = mcolor * texture(fontTexData, vec3(texCoord, texNum));
+        if (bool(b)) {
+            fragColor = max(texture(fontTexData, vec3(texCoord, texNum)), texture(fontTexData, vec3(vec2(texCoord.x - scale / chartw, texCoord.y), texNum)));
+        } else {
+            fragColor = texture(fontTexData, vec3(texCoord, texNum));
+        }
+        if (bool(u) && int(charty) >= int(charth) - scale) {
+            fragColor = mcolor;
+        }
+        if (bool(s) && int(charty) >= int(charth / 2.0) - scale && int(charty) < int(charth / 2.0)) {
+            fragColor = mcolor;
+        }
+        fragColor *= mcolor;
         fragColor = mix(bmcolor, fragColor, fragColor.a);
         if (fragColor.a == 0.0) discard;
     } else {
--- resources/base/engine/shaders/code/GLSL/ui/vertex.glsl
+++ resources/base/engine/shaders/code/GLSL/ui/vertex.glsl
@@ -3,7 +3,7 @@ layout (location = 0) in uint data1;
 // text:    [16 bits: x][16 bits: y]
 layout (location = 1) in uint data2;
 // ui elem: [1 bit: 0][23 bits: reserved][8 bits: z]
-// text:    [1 bit: 1][8 bits: reserved][8 bits: reserved (16-bit charset maybe)][8 bits: text char][8 bits: z]
+// text:    [1 bit: 1][4 bits: reserved][1 bit: strikethrough][1 bit: underline][1 bit: italic][1 bit: bold][8 bits: reserved (16-bit charset maybe)][8 bits: text char][8 bits: z]
 layout (location = 2) in uint data3;
 // ui elem: [8 bits: r][8 bits: g][8 bits: b][8 bits: a]
 // text:    [8 bits: fgc alpha][8 bits: bgc alpha][4 bits: text fgc][4 bits: text bgc][8 bits: reserved]
@@ -14,22 +14,36 @@ uniform float xsize;
 uniform float ysize;
 uniform vec3 textColor[16];
 uniform vec2 uitexdiv;
+uniform float scale;
 
-out vec2 texCoord;
 out float texNum;
 out vec4 mcolor;
 out vec4 bmcolor;
+out float chartw;
+out float charth;
+out float chartx;
+out float charty;
 flat out uint elemType;
+flat out uint b;
+flat out uint u;
+flat out uint s;
 
 void main() {
-    float x = float(int(-32768) * int((data1 >> 31) & uint(1)) + int((data1 >> 16) & uint(32767))) / xsize;
-    float y = float(int(-32768) * int((data1 >> 15) & uint(1)) + int(data1 & uint(32767))) / ysize;
+    float x = float(int(-32768) * int((data1 >> 31) & uint(1)) + int((data1 >> 16) & uint(32767)));
+    float y = float(int(-32768) * int((data1 >> 15) & uint(1)) + int(data1 & uint(32767)));
     float z = 1.0 - (float(int(-128) * int((data2 >> 7) & uint(1)) + int(data2 & uint(127))) / 256.0 + 0.5);
-    gl_Position = vec4(x * 2.0 - 1.0, 1.0 - y * 2.0, z, 1.0);
     if ((elemType = ((data2 >> 31) & uint(1))) == uint(1)) {
         texNum = float((data2 >> 8) & uint(255));
-        texCoord.x = float((data4 >> 24) & uint(255)) / float((data4 >> 8) & uint(255));
-        texCoord.y = float((data4 >> 16) & uint(255)) / float(data4 & uint(255));
+        chartw = float((data4 >> 8) & uint(255));
+        charth = float(data4 & uint(255));
+        chartx = float((data4 >> 24) & uint(255));
+        charty = float((data4 >> 16) & uint(255));
+        b = (data2 >> 24) & uint(1);
+        if (bool((data2 >> 25) & uint(1))) {
+            x += (1.0 - ((charty / charth) * 2.0)) * 4.0 * scale;
+        }
+        u = (data2 >> 26) & uint(1);
+        s = (data2 >> 27) & uint(1);
         mcolor = vec4(textColor[uint((data3 >> 12) & uint(15))], float((data3 >> 24) & uint(255)) / 255.0);
         bmcolor = vec4(textColor[uint((data3 >> 8) & uint(15))], float((data3 >> 16) & uint(255)) / 255.0);
     } else {
@@ -38,4 +52,5 @@ void main() {
         mcolor.b = float((data3 >> 8) & uint(255)) / 255.0;
         mcolor.a = float(data3 & uint(255)) / 255.0;
     }
+    gl_Position = vec4((x * 2.0) / xsize - 1.0, 1.0 - (y * 2.0) / ysize, z, 1.0);
 }
