23f16d6602ac7925fbf1a3bff5b7da6dcadb719b
PQCraft <0456523@gmail.com>
2022-12-02 18:00:42 -0500

    Server optimization test

--- src/server/server.h
+++ src/server/server.h
@@ -11,31 +11,31 @@
 #endif
 
 #ifndef SERVER_SNDBUF_SIZE
-    #define SERVER_SNDBUF_SIZE (1 << 19)
+    #define SERVER_SNDBUF_SIZE (1 << 20)
 #endif
 
 #ifndef SERVER_RCVBUF_SIZE
-    #define SERVER_RCVBUF_SIZE (1 << 18)
+    #define SERVER_RCVBUF_SIZE (1 << 19)
 #endif
 
 #ifndef CLIENT_SNDBUF_SIZE
-    #define CLIENT_SNDBUF_SIZE (1 << 18)
+    #define CLIENT_SNDBUF_SIZE (1 << 19)
 #endif
 
 #ifndef CLIENT_RCVBUF_SIZE
-    #define CLIENT_RCVBUF_SIZE (1 << 19)
+    #define CLIENT_RCVBUF_SIZE (1 << 20)
 #endif
 
 #ifndef SERVER_OUTBUF_SIZE
-    #define SERVER_OUTBUF_SIZE (1 << 20)
+    #define SERVER_OUTBUF_SIZE (1 << 19)
 #endif
 
 #ifndef SERVER_INBUF_SIZE
-    #define SERVER_INBUF_SIZE (1 << 18)
+    #define SERVER_INBUF_SIZE (1 << 19)
 #endif
 
 #ifndef CLIENT_OUTBUF_SIZE
-    #define CLIENT_OUTBUF_SIZE (1 << 16)
+    #define CLIENT_OUTBUF_SIZE (1 << 21)
 #endif
 
 #ifndef CLIENT_INBUF_SIZE
