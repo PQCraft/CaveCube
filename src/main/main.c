77f3a53ba01e075471ad4a26c187bf222486602b
PQCraft <0456523@gmail.com>
2023-03-28 13:09:06 -0400

    Progress update

--- src/main/main.c
+++ src/main/main.c
@@ -73,16 +73,15 @@ static inline bool altchdir(char* path) {
     }\
 }
 
-int main(int _argc, char** _argv) {
-    #if defined(_WIN32) && MODULEID == MODULEID_GAME
+#if MODULEID == MODULEID_GAME
+int game_main() {
+    #if defined(_WIN32)
         DWORD procs[2];
         DWORD procct = GetConsoleProcessList((LPDWORD)procs, 2);
         bool owncon = (procct < 2);
         if (owncon) ShowWindow(GetConsoleWindow(), SW_HIDE);
         QueryPerformanceFrequency(&perfctfreq);
     #endif
-    argc = _argc;
-    argv = _argv;
     #ifndef __EMSCRIPTEN__
         maindir = strdup(pathfilename(execpath()));
     #else
@@ -91,35 +90,29 @@ int main(int _argc, char** _argv) {
     startdir = realpath(".", NULL);
     MAIN_STRPATH(startdir);
     #ifndef __EMSCRIPTEN__
-        #if MODULEID == MODULEID_GAME
-            {
-                #ifndef _WIN32
-                    char* tmpdir = getenv("HOME");
-                #else
-                    char* tmpdir = getenv("AppData");
-                #endif
-                char tmpdn[MAX_PATH] = "";
-                strcpy(tmpdn, tmpdir);
-                strcat(tmpdn, "/.cavecube");
-                if (!md(tmpdn)) return 1;
-                if (!altchdir(tmpdn)) return 1;
-                if (!md("worlds")) return 1;
-                if (!md("resources")) return 1;
-                localdir = realpath(".", NULL);
-                MAIN_STRPATH(localdir);
-            }
-            strcpy(configpath, localdir);
-            strcat(configpath, "config.cfg");
-        #else
-            strcpy(configpath, "ccserver.cfg");
-        #endif
+        {
+            #ifndef _WIN32
+                char* tmpdir = getenv("HOME");
+            #else
+                char* tmpdir = getenv("AppData");
+            #endif
+            char tmpdn[MAX_PATH] = "";
+            strcpy(tmpdn, tmpdir);
+            strcat(tmpdn, "/.cavecube");
+            if (!md(tmpdn)) return 1;
+            if (!altchdir(tmpdn)) return 1;
+            if (!md("worlds")) return 1;
+            if (!md("resources")) return 1;
+            localdir = realpath(".", NULL);
+            MAIN_STRPATH(localdir);
+        }
+        strcpy(configpath, localdir);
+        strcat(configpath, "config.cfg");
     #endif
     #if DBGLVL(1)
         printf("Main directory: {%s}\n", maindir);
         printf("Start directory: {%s}\n", startdir);
-        #if MODULEID == MODULEID_GAME
-            printf("Local directory: {%s}\n", localdir);
-        #endif
+        printf("Local directory: {%s}\n", localdir);
         printf("Config path: {%s}\n", configpath);
     #endif
     if (!altchdir(maindir)) return 1;
@@ -138,11 +131,7 @@ int main(int _argc, char** _argv) {
     }
     if (!altchdir(maindir)) exit(1);
     #ifdef _WIN32
-        #if MODULEID == MODULEID_GAME
-            declareConfigKey(config, "Main", "showConsole", "false", false);
-        #else
-            declareConfigKey(config, "Main", "showConsole", "true", false);
-        #endif
+        declareConfigKey(config, "Main", "showConsole", "false", false);
         showcon = getBool(getConfigKey(config, "Main", "showConsole"));
     #endif
     initResource();
@@ -154,19 +143,23 @@ int main(int _argc, char** _argv) {
     #ifdef _WIN32
         if (owncon && showcon) ShowWindow(GetConsoleWindow(), SW_SHOW);
     #endif
-    #if MODULEID == MODULEID_GAME
-        if (!doGame()) return 1;
-    #else
-    #endif
-    #if MODULEID == MODULEID_GAME
-        if (!altchdir(startdir)) return 1;
-        if (config->changed) if (!writeConfig(config, configpath)) return 1;
-    #endif
+    if (!doGame()) return 1;
+    if (!altchdir(startdir)) return 1;
+    if (config->changed) if (!writeConfig(config, configpath)) return 1;
     closeConfig(config);
     free(maindir);
     free(startdir);
-    #if MODULEID == MODULEID_GAME
-        free(localdir);
-    #endif
+    free(localdir);
     return 0;
+}
+#endif
+
+int main(int _argc, char** _argv) {
+    argc = _argc;
+    argv = _argv;
+    #if MODULEID == MODULEID_GAME
+        return game_main();
+    #else
+        return server_main();
+    #endif
 }
